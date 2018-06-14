ee1b59c3f0a70413589e17c4c5cfbfda8eb0396b
Hallee Moon <pi.is.good.3.141@gmail.com>
2018-06-13 19:31:24 -0700

    add start function for ease of reseting the game.

--- js/app.js
+++ js/app.js
@@ -10,29 +10,26 @@ let startTime=0;
 let timerId = 0;
 
 document.addEventListener("DOMContentLoaded", function(){
+  start();
+});
+
+// use this function to set up the page in the beginning and to reset the page.
+function start(){
+  openCards = [];
+  moves = 0;
+  matches = 0;
+  starCount =3;
+  seconds=0;
+  minutes=0;
+  startTime=0;
+  timerId = 0;
+
   cardDesign=shuffle(cardDesign);
-  console.log(cardDesign);
   makeCards(cardDesign);
   let allCards = document.querySelectorAll('.card');
   init(allCards);//this sets up the event listeners on the cards and starts checking them.
-});
 
-
-function makeCards(cardDesign){
-  const myDeck = document.querySelector('.deck');
-  for (let j = 0; j < 16; j++) {
-    const newElement = document.createElement('li');
-    newElement.classList.add('card');
-    newElement.innerHTML = '<i class= "' + cardDesign[j]+ '"> </i>';
-    myDeck.appendChild(newElement);
-  }
 };
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -47,19 +44,17 @@ function shuffle(array) {
     }
 
     return array;
-}
+};
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+function makeCards(cardDesign){
+  const myDeck = document.querySelector('.deck');
+  for (let j = 0; j < 16; j++) {
+    const newElement = document.createElement('li');
+    newElement.classList.add('card');
+    newElement.innerHTML = '<i class= "' + cardDesign[j]+ '"> </i>';
+    myDeck.appendChild(newElement);
+  }
+};
 
 function init(allCards){
  allCards.forEach(function(card){
